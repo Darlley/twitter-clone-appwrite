9049dc46b1f113c3b9bc91069a444e651b82c30c
Darlley <sytembrtt@gmail.com>
2023-12-07 16:52:36 -0400

    setup login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,164 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
+
+import { Client, Account, Databases } from 'appwrite';
+import Logo from "../components/Logo"
+import { Button, Input, Tab, Tabs } from '@nextui-org/react';
+import Link from 'next/link';
+import { useCallback, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({ tweets }) {
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  const [createEmail, setCreateEmail] = useState("");
+  const [createPassword, setCreatePassword] = useState("");
+  const [createName, setCreateName] = useState("");
+
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  const [selected, setSelected] = useState("sign-up");
+
+  // useCallback(() => {}, [loggedInUser])
+
+  // function async create user signup for AppWrite
+  const register = async () => {
+    const client = new Client();
+    const account = new Account(client);
+
+    client
+      .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT)
+      .setProject(process.env.NEXT_PUBLIC_PROJECT);
+
+    const response = account.create(createEmail, createPassword, createName);
+
+    response.then(function (response) {
+      console.log(response)
+    }, function (error) {
+      console.log(error)
+    })
+  };
+
+  const login = async () => {
+    const client = new Client();
+    const account = new Account(client);
+
+    client
+      .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT)
+      .setProject(process.env.NEXT_PUBLIC_PROJECT);
+
+    const response = account.createEmailSession(loginEmail, loginPassword);
+
+    response.then(function (response) {
+      console.log(response)
+    }, function (error) {
+      console.log(error)
+    })
+  };
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      className={`flex min-h-screen md:flex-row flex-col items-center p-4 md:p-0 justify-center bg-gray-50 ${inter.className}`}>
+      <section className='flex-1 flex items-center justify-center'>
+        <Logo classes="md:h-96 h-60 w-auto" />
+      </section>
+      <section className='flex-1 flex items-center justify-center'>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="flex w-full flex-col items-start justify-start min-h-[550px] md:max-w-[65%]">
+        <h1 className='text-5xl mb-8'>Acontecendo agora</h1>
+
+        <Tabs 
+          aria-label="Options" 
+          color="primary" 
+          variant="bordered" 
+          selectedKey={selected}
+          onSelectionChange={setSelected}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <Tab key="sign-up" title="Sign up" className='w-full'>
+            <div className='flex flex-col gap-2 w-full '>
+              <h2 className='text-xl'>Cadastre-se no Twitter</h2>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+              <Input
+                type="text"
+                label="Name"
+                value={createName}
+                onChange={(e) => setCreateName(e.target.value)}
+              />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+              <Input
+                type="email"
+                label="Email"
+                value={createEmail}
+                onChange={(e) =>
+                setCreateEmail(e.target.value)}
+              />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+              <Input
+                type="password"
+                label="Password"
+                value={createPassword}
+                onChange={(e) => setCreatePassword(e.target.value)}
+              />
+
+              <Button color="primary" variant="shadow" onClick={() => register()}>
+                Cadastrar
+              </Button>  
+
+              <span className='mt-4 text-gray-500 text-sm'>Já tem uma conta?
+                <button className='ml-2 inline-block font-bold text-blue-500' onClick={() => setSelected('login')}>Faça login</button>
+              </span>
+            </div>
+          </Tab>
+
+          <Tab key="login" title="Login" className='w-full'>
+            <div className='flex flex-col gap-2 w-full'>
+              <h2 className='text-xl'>Faça seu login</h2>
+              <Input 
+                type="email" 
+                label="Email" 
+                value={loginEmail}
+                onChange={(e) => setLoginEmail(e.target.value)} 
+              />
+              <Input 
+                type="password" 
+                label="Password" 
+                value={loginPassword}
+                onChange={(e) => setLoginPassword(e.target.value)}
+              />
+
+              <Button color="primary" variant="shadow" onClick={() => login()}>
+                Entrar
+              </Button>  
+
+              <span className='mt-4 text-gray-500 text-sm'>Não tem uma conta?
+                <button className='ml-2 inline-block font-bold text-blue-500' onClick={() => setSelected('sign-up')}>Faça seu cadastro</button>
+              </span>
+            </div>
+          </Tab>
+        </Tabs>
+      </div>  
+      </section>
     </main>
   )
+}
+
+export async function getServerSideProps(context) {
+  const client = new Client();
+
+  client
+    .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT)
+    .setProject(process.env.NEXT_PUBLIC_PROJECT);
+
+  const databases = new Databases(client);
+
+  const tweets = await databases.listDocuments(
+    process.env.NEXT_PUBLIC_DATABASE,
+    process.env.NEXT_PUBLIC_TWEETS_COLLECTION,
+  );
+
+  return {
+    props: { tweets }
+  }
 }
